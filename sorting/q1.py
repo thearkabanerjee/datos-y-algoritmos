61fa5d9bce78166dcc7980e9e43dd89ad9a4b869
Arka Banerjee <arkabanerjee381@gmail.com>
2026-09-02 09:57:06 +0530

    q1.py

---
+++ sorting/q1.py
@@ -0,0 +1,29 @@
+# Write:
+
+# def selection_sort(numbers):
+#     # your code
+
+# Sort this list in ascending order:
+
+numbers = [7, 3, 9, 2, 5]
+
+# Expected output:
+
+# [2, 3, 5, 7, 9]
+
+
+def selection_sort(numbers):
+    size = len(numbers)
+
+    for i in range (size):
+        min_index = i
+
+        for j in range (i+1, size):
+            if (numbers[j] < numbers[min_index]):
+                min_index = j
+
+        numbers[min_index], numbers[i] = numbers[i],numbers[min_index]
+
+    return numbers
+
+print (selection_sort(numbers))
