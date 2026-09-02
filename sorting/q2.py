32a6dc944aafe7cf8ee35c997608a735a3da7b13
Arka Banerjee <arkabanerjee381@gmail.com>
2026-09-02 10:19:08 +0530

    q2.py

--- sorting/q2.py
+++ sorting/q2.py
@@ -12,13 +12,13 @@ def selection_sort(array):
     size = len(array)
 
     for i in range(size):
-        min_index = i
+        max_index = i
 
         for j in range (i+1, size):
-            if (array[j] > array[min_index]):
-                min_index = j 
+            if (array[j] > array[max_index]):
+                max_index = j 
 
-        array[i], array[min_index] = array[min_index], array[i]
+        array[i], array[max_index] = array[max_index], array[i]
    
     return array
 
